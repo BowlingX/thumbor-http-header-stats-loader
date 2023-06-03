f4e06359279a136194e44401073fae47e3817644
David Heidrich <me@bowlingx.com>
2023-06-03 14:31:13 +0200

    chore: adjusted descriptions

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ def readme():
 
 setup(
     name='thumbor_hsl',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
-    url='https://github.com/BowlingX/thumbor-http-header-stats-loader',
+    url='https://github.com/BowlingX/thumbor-http-stats-loader',
     license='MIT',
     author='David Heidrich',
     author_email='me@bowlingx.com',
     install_requires=['thumbor>=7.0.0a2,<8'],
-    description='A modified `http` loader for thumbor which introduces an additional statd counter to track headers',
+    description='A modified `http` loader for thumbor which provides additional statistics '
+                '(like width, height and url parsing)',
     long_description=readme(),
 )
